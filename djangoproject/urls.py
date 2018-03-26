9af6a5f8714c91ba24f677c235554ecaa3f91995
sheilambadi <mbadiatieno@gmail.com>
2018-03-26 14:34:19 +0300

    redirected base route to posts

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    url(r'^$', include('posts.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls')),
 ]
