d7408d163428f2eaa0da36840a3b36e032920484
sheilambadi <mbadiatieno@gmail.com>
2018-03-26 14:46:13 +0300

    display post titles  in html

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Posts
+
 # Create your views here.
 
 def index(request):
     #display in browser
     #return HttpResponse('Hello Django!!!')
 
+    #display posts in view
+    posts = Posts.objects.all()[:10]
+
+    context = {
+        'title' : 'Latest Posts',
+        'posts' : posts
+    }
     #render from template
-    return render(request, 'posts/index.html', {
-        'title' : 'Latest Posts'
-    })
+    return render(request, 'posts/index.html', context)
